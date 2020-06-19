eaa40f28a767d613bf97078fa072f2354a0ef296
pavankumar-derangula <67139471+pavankumar-derangula@users.noreply.github.com>
2020-06-19 14:51:50 +0530

    Add files via upload

---
+++ emp.js
@@ -0,0 +1,22 @@
+//add button onclick open form
+
+
+function openForm() {
+    document.getElementById("popupForm").style.display="block";
+  }
+  
+  function closeForm() {
+    document.getElementById("popupForm").style.display="none";
+  }
+ /* window.onclick = function(event) {
+    var modal = document.getElementById('loginPopup');
+    if (event.target == modal) {
+closeForm();
+*/
+
+
+//delete button on click delete employee row
+
+
+
+//edit button on click open form with fields with previous data
