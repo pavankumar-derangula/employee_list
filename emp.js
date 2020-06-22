636f8818dedfc521f9aeded96e757c678ef7daca
pavankumar-derangula <67139471+pavankumar-derangula@users.noreply.github.com>
2020-06-23 00:27:34 +0530

    post method implemented

--- emp.js
+++ emp.js
@@ -6,6 +6,10 @@ function openForm() {
   }
   
   function closeForm() {
+  x=document.getElementById('formData');
+    x.elements[0].value='';
+    x.elements[1].value='';
+    x.elements[2].value='';
     document.getElementById("popupForm").style.display="none";
   }
 
@@ -44,38 +48,103 @@ function uiUpdate(update){
     cell2.innerHTML=update.data[i]["employee_name"];
     cell3.innerHTML=update.data[i]["employee_salary"];
     cell4.innerHTML=update.data[i]["employee_age"];
+  }
+
 }
 
+
+function verify(data){
+  if(data.elements[0].value.length==0||data.elements[1].value.length==0||data.elements[2].value.length==0 ){
+    window.alert('fields should not be empty..!')
+    return false;
+  }
+
+  if(data.elements[0].value.match(/[^A-Za-z]+/g)!=null)
+  {
+    window.alert('Name should only contain alphabets');
+    return false;
+  }
+  if(data.elements[1].value<1){
+    window.alert('salary should be >1');
+    return false;
+
+  }
+  if(data.elements[2].value<18){
+  window.alert('Age should be > 18');
+  return false;
+  }
+  else{
+    return true;
+  }
 }
 
 //add employee to ui.......
-function addEmpData(){
+
+async function addEmpData()
+{
    
-  var x=document.getElementById('formData')
-  
-  const id=x.elements[0].value;
-  for(let i=0;i<checkId.length;i++)
-  {
-    if(id===checkId[i]){
-      alert('Given employee id already exits,please enter new id..!');
-      return;
+    var x=document.getElementById('formData');
+    
+  // const id=x.elements[0].value;
+  /* for(let i=0;i<checkId.length;i++)
+    {
+      if(id===checkId[i]){
+        alert('Given employee id already exits,please enter new id..!');
+        return;
+      }
+    } */
+    var data={
+      name : x.elements[0].value,
+      salary : x.elements[1].value,
+      age : x.elements[2].value
+    };
+    
+    if(verify(x)){
+      
+      var urlParams = new URLSearchParams(window.location.search);
+      var id = urlParams.get('id');
+      var add=await fetch('http://dummy.restapiexample.com/api/v1/create',{
+        method : 'POST',
+        mode : 'cors',
+        cache: 'no-cache',
+        credentials:'same-origin',
+        headers:{
+          'Content-Type':'application/json'
+        },
+        redirect:'follow',
+        referrerPolicy:'no-referrer',
+        body: JSON.stringify(data)
+
+      });
+      var check= await add.json();
+      
+      if(check.status=='success')
+      {
+        window.alert('Employee added..!');
+       // addToTable();
+        
+        checkId.push(++(checkId[checkId.length-1]));
+        var table=document.getElementById('myTable');
+        var row=table.insertRow(-1);
+        var cell1=row.insertCell(0);
+        var cell2=row.insertCell(1);
+        var cell3=row.insertCell(2);
+        var cell4=row.insertCell(3);
+        cell1.innerHTML=checkId[checkId.length-1];
+        cell2.innerHTML=x.elements[0].value;
+        cell3.innerHTML=x.elements[1].value;
+        cell4.innerHTML=x.elements[2].value;
+        closeForm();
+      }
+      else{
+        window.alert('Sorry failed to add employee..!');
+      }
+      
+    
     }
-  } 
-    checkId.push(id);
-    var table=document.getElementById('myTable');
-    var row=table.insertRow(-1);
-    var cell1=row.insertCell(0);
-    var cell2=row.insertCell(1);
-    var cell3=row.insertCell(2);
-    var cell4=row.insertCell(3);
-    cell1.innerHTML=x.elements[0].value;
-    cell2.innerHTML=x.elements[1].value;
-    cell3.innerHTML=x.elements[2].value;
-    cell4.innerHTML=x.elements[3].value;
-    closeForm();
-  
 
 }
+
 
 
 
