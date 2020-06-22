2129c8223b1d28edf590ab28a702620ef17ab600
pavankumar-derangula <67139471+pavankumar-derangula@users.noreply.github.com>
2020-06-22 14:33:32 +0530

    changes

--- emp.js
+++ emp.js
@@ -8,15 +8,91 @@ function openForm() {
   function closeForm() {
     document.getElementById("popupForm").style.display="none";
   }
- /* window.onclick = function(event) {
-    var modal = document.getElementById('loginPopup');
-    if (event.target == modal) {
-closeForm();
-*/
 
 
-//delete button on click delete employee row
+var checkId=new Array();
+ 
+ //get data from api...
+
+ async function getData(){
+   
+   const fetchResult=await fetch('http://dummy.restapiexample.com/api/v1/employees');
+   const fetchData=await fetchResult.json();
+
+   uiUpdate(fetchData);
+ }
+
+
+getData();
+
+//add fetched_data to ui...
+
+function uiUpdate(update){
+  
+  var table=document.getElementById('myTable');
+
+  for(let i=0;i<update.data.length;i++){
+    
+    
+    checkId.push(update.data[i]["id"]);
+    var row=table.insertRow(-1);
+    var cell1=row.insertCell(0);
+    var cell2=row.insertCell(1);
+    var cell3=row.insertCell(2);
+    var cell4=row.insertCell(3);
+    cell1.innerHTML=update.data[i]["id"];
+    cell2.innerHTML=update.data[i]["employee_name"];
+    cell3.innerHTML=update.data[i]["employee_salary"];
+    cell4.innerHTML=update.data[i]["employee_age"];
+}
+
+}
+
+//add employee to ui.......
+function addEmpData(){
+   
+  var x=document.getElementById('formData')
+  
+  const id=x.elements[0].value;
+  for(let i=0;i<checkId.length;i++)
+  {
+    if(id===checkId[i]){
+      alert('Given employee id already exits,please enter new id..!');
+      return;
+    }
+  } 
+    checkId.push(id);
+    var table=document.getElementById('myTable');
+    var row=table.insertRow(-1);
+    var cell1=row.insertCell(0);
+    var cell2=row.insertCell(1);
+    var cell3=row.insertCell(2);
+    var cell4=row.insertCell(3);
+    cell1.innerHTML=x.elements[0].value;
+    cell2.innerHTML=x.elements[1].value;
+    cell3.innerHTML=x.elements[2].value;
+    cell4.innerHTML=x.elements[3].value;
+    closeForm();
+  
+
+}
 
 
 
-//edit button on click open form with fields with previous data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
